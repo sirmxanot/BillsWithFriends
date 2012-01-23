580ddc85711e216b7a8641c6a84edf3fee7057ea
Andrew Frank <andrewpaulfrank@gmail.com>
2012-01-23 16:28:28 -0500

    Added named scope to Expenses and Payments to find records created by current user. Deleted files pertaining to deprecated person object.

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,3 @@
 class Expense < ActiveRecord::Base
+	scope :user_owned, expenses.where(expense[:owner_id].eq(current_user.id))
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,3 @@
 class Payment < ActiveRecord::Base
+	scope :user_owned, payments.where(payment[:owner_id].eq(current_user.id))
 end
