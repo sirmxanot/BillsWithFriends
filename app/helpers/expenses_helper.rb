6ef4e81567efe89ba1a691af20270f141adac9d8
Andrew Frank <andrewpaulfrank@gmail.com>
2012-01-26 17:59:30 -0500

    Formatting change to expenses_helper and got rid of validates presence of payment_id because payment_id wont be created yet when we create the i_paid_you

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -3,6 +3,6 @@ def setup_expense(expense)
     expense.you_owe_mes ||= You_owe_me.new
     1.times { expense.you_owe_mes.build }
     expense
-  end
+end
 
 end
--- app/models/i_paid_you.rb
+++ app/models/i_paid_you.rb
@@ -1,7 +1,7 @@
 class IPaidYou < ActiveRecord::Base
 	belongs_to :payment
 
-	validates_presence_of :paymnent_id, :amount, :payer_id, :receiver_id
+	validates_presence_of :amount, :payer_id, :receiver_id
 	validates :amount, :numericality => {:greater_than_or_equal_to => 1}
 	
 	require 'Money'
