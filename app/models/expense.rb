52d2c3f381eb29efb50c93de60f331e355864181
Andrew Frank <andrewpaulfrank@gmail.com>
2012-01-26 13:32:14 -0500

    Added accepts_nested_attributes_for their dependent classes (i_paid_yous and you_owe_mes)

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,7 @@
 class Expense < ActiveRecord::Base
 	belongs_to :user
 	has_many :you_owe_mes
+	accepts_nested_attributes_for :you_owe_mes
 	
 
 	validates_presence_of :expense_name, :owner_id, :date_incurred, :total_amount
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,7 @@
 class Payment < ActiveRecord::Base
 	belongs_to :user
 	has_one :i_paid_you
+	accepts_nested_attributes_for :i_paid_you
 
 	validates_presence_of :owner_id, :date_paid, :total_amount
 	validates :total_amount, :numericality => {:greater_than_or_equal_to => 1}
