89a4244dc78a5600949a5367f69a6c982e2a6f05
Andrew Frank <andrewpaulfrank@gmail.com>
2012-01-26 17:54:05 -0500

    Got rid of validates presence of expense_id, because the associated expense wont yet have an id when the you_owe_me is created.

--- app/models/you_owe_me.rb
+++ app/models/you_owe_me.rb
@@ -1,7 +1,7 @@
 class YouOweMe < ActiveRecord::Base
 	belongs_to :expense
 
-	validates_presence_of :expense_id, :amount, :creditor_id, :debtor_id
+	validates_presence_of :amount, :creditor_id, :debtor_id
 	validates :amount, :numericality => {:greater_than_or_equal_to => 1}
 
 	require 'Money'
