afa8735cc342be10cade44232addc7b4fc31d428
Andrew Frank <andrewpaulfrank@gmail.com>
2012-01-30 22:43:43 -0500

    Updated javascript settings from "application" to :default

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+function remove_fields (link) {
+	$(link).previous("input[type=hidden]").value = "1";
+	$(link).up(".fields").hide();
+}
