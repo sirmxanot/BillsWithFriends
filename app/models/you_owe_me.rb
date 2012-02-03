b92c75945ff8edeb177c8929f893440cae3110c3
Andrew Frank <andrewpaulfrank@gmail.com>
2012-02-03 12:25:29 -0500

    1) models/register: got rid of attr_accessible for credit_extended. un-necessary 2) models/you_owe_me: refactored code that updates register.credit_extended before a you_owe_me is saved. code now works 3) views/registers/index: got rid of add new register link

--- app/models/you_owe_me.rb
+++ app/models/you_owe_me.rb
@@ -1,4 +1,5 @@
 class YouOweMe < ActiveRecord::Base
+	#Calls the update_register method before saving a you_owe_me
 	before_save :update_register
 
 	belongs_to :expense
@@ -7,24 +8,16 @@ class YouOweMe < ActiveRecord::Base
 
 	validates_presence_of :amount, :creditor_id, :debtor_id
 	validates :amount, :numericality => {:greater_than_or_equal_to => 1}
+	#Need to add a validation to ensure that you cannot submit a you_owe_me where the debtor and creditor ids are the same
 
 	private
-	  def update_register
-	  	creditor_id = self.creditor_id
-	  	debtor_id = self.debtor_id
 
-	    if Register.where(:creditor_id => creditor_id, :debtor_id => debtor_id).empty?
-				register = Register.new(:creditor_id => creditor_id, :debtor_id => debtor_id)
-	    else
-	    	register = Register.where(:creditor_id => creditor_id, :debtor_id => debtor_id)
-	    end
+	#This method creates a register if it doesn't exist and then adds the you_owe_me.amount to the register.credit_extended
+  def update_register
+  	register = Register.where(:creditor_id => creditor_id, :debtor_id => debtor_id).first
+  	register ||= Register.new(:creditor_id => creditor_id, :debtor_id => debtor_id)
 
-	    register.credit_extended += self.amount
-
-	    #credit_extended = register.credit_extended
-	    #credit_extended += self.amount
-	    #register.update_attributes(:credit_extended => credit_extended)
-
-	    register.save
-	  end
+    credit_extended = register.credit_extended + self.amount
+    register.update_attributes(:credit_extended => credit_extended)
+  end
 end
