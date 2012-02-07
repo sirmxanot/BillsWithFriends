904db67bcac06360eb419e447932eb6dd0d69449
Andrew Frank <andrewpaulfrank@gmail.com>
2012-02-07 17:04:05 -0500

    Deleting files related to i_paid_yous. The i_paid_you attributes are now represented within the payment directly.

--- test/unit/helpers/i_paid_yous_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class IPaidYousHelperTest < ActionView::TestCase
-end
--- test/unit/i_paid_you_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class IPaidYouTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
