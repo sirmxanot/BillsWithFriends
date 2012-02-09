dcb25f96b2c6d4f78e29a6d5571c5d6cc96c131c
Andrew Frank <andrewpaulfrank@gmail.com>
2012-02-09 18:21:25 -0500

    1) Models/expense_validator: Deleted unnecessary code 2) Models/expense: added note to self in comment

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -10,7 +10,8 @@ class Expense < ActiveRecord::Base
 								  :reject_if => lambda { |a| a[:debtor_id].blank? }
 	# in the future, refactor this lambda so that it also checks if the amount field is blank
 
-	validates_presence_of :expense_name, :user_id, :date_incurred, :total_amount
+	# need to add a validation that you_owe_mes still exist after an update occurs
+	validates_presence_of :expense_name, :user_id, :date_incurred, :total_amount, :you_owe_mes
 	validates :total_amount, :numericality => {:greater_than_or_equal_to => 1}
 	validates_with ExpenseValidator
 end
