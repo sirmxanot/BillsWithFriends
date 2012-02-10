e0b6257ad568b29eba3232363aeb06e1a7b6787a
Andrew Frank <andrewpaulfrank@gmail.com>
2012-02-09 19:30:36 -0500

    added validation to prevent people from recording payments that occur in the future

--- app/models/payment_validator.rb
+++ app/models/payment_validator.rb
@@ -1,9 +1,12 @@
 class PaymentValidator < ActiveModel::Validator
   
   def validate(record)
-  
   	if do_you_owe_checker(record)
   		record.errors[:base] << "You don't owe them anything! You can't record payments for those who you do not owe anything."
+  	end
+
+  	if future_expense_checker(record)
+  		record.errors[:base] << "You can't record payments for a time in the future"
   	end
 	end
 
@@ -12,6 +15,13 @@ class PaymentValidator < ActiveModel::Validator
 	#Makes sure user isn't submitting a payment for someone that they don't owe anything.
 	def do_you_owe_checker (record)
 		Register.register_for_payment(record.receiver_id, record.user_id).first.nil?
+  end
+
+  #Makes sure user isn't submitting a payment that occurs in the future.
+	def future_expense_checker (record)
+		date = record.date_paid
+  	time = date.to_time
+  	time.future?
   end
 
 end
