274c1b5e4971204c8a1a2574619ba2391174614a
Andrew Frank <andrewpaulfrank@gmail.com>
2012-02-11 14:35:29 -0500

    redirect after user login to summary and after logout to login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  # Overwriting the sign_in redirect path method
+  def after_sign_in_path_for(resource_or_scope)
+    summary_path
+  end
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    login_path
+  end
 end
