1f9fc2192222d12f3312c8ffb931b8c8af20a339
Andrew Frank <andrewpaulfrank@gmail.com>
2012-02-16 12:41:05 -0500

    Added validation to make sure that all of the component you_owe_me.amounts sum to the total_amount of the expense

--- app/models/expense_validator.rb
+++ app/models/expense_validator.rb
@@ -4,6 +4,10 @@ class ExpenseValidator < ActiveModel::Validator
   	if future_expense_checker(record)
   		record.errors[:base] << "You can't record expenses for a time in the future"
   	end
+
+    if amounts_equal_total_amount(record)
+      record.errors[:base] << "The total of what each individual owes you has to equal the total amount of the expense"
+    end
 	end
 
 private
@@ -12,5 +16,14 @@ private
 		date = record.date_incurred
   	time = date.to_time
   	time.future?
+  end
+
+  #Makes sure that the amount of all the you_owe_mes add up to the total_amount of the expense
+  def amounts_equal_total_amount (record)
+    amount = 0
+    record.you_owe_mes.each do |you_owe_me|
+      amount += you_owe_me.amount
+    end
+    return 1 if amount != record.total_amount
   end
 end
