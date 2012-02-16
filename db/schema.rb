c0949ff63bc0d74c2d4b4779db55a21f7592aa3d
Andrew Frank <andrewpaulfrank@gmail.com>
2012-02-16 12:43:43 -0500

    removed the deprecated email_address attribute

--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(:version => 20120207215551) do
   create_table "users", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email_address"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email",                                 :default => "", :null => false
