7f3d6708c3118feaa28519ba8e2a0f4ad1bfbd03
Andrew Frank <andrewpaulfrank@gmail.com>
2012-02-29 12:50:40 -0500

    Prevent users from editing, updating, and deleting registers they don't own. Prevent users from viewing (index or show) registers that they're not a participant in Deleted register edit view Eliminated edit and destroy links in register index

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -4,7 +4,11 @@ class RegistersController < ApplicationController
   # GET /registers
   # GET /registers.json
   def index
-    @registers = Register.all
+    @owes_user = Register.current_user_owns(current_user.id)
+    @user_owes = Register.current_user_owes(current_user.id)
+    
+    @unsorted_registers = @owes_user + @user_owes
+    @registers = @unsorted_registers.sort_by {|item| item.updated_at}.reverse
 
     respond_to do |format|
       format.html # index.html.erb
@@ -17,10 +21,14 @@ class RegistersController < ApplicationController
   def show
     @register = Register.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @register }
-    end
+    if @register.creditor_id != current_user.id && @register.debtor_id != current_user.id
+      flash[:error] = "You can't view a register for which you are not either the creditor or the debtor."
+      begin
+        redirect_to :back
+      rescue Exception
+        redirect_to summary_url
+      end
+    end 
   end
 
   def audit
@@ -39,66 +47,6 @@ class RegistersController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @register }
-    end
-  end
-
-  # GET /registers/new
-  # GET /registers/new.json
-  def new
-    @register = Register.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @register }
-    end
-  end
-
-  # GET /registers/1/edit
-  def edit
-    @register = Register.find(params[:id])
-  end
-
-  # POST /registers
-  # POST /registers.json
-  def create
-    @register = Register.new(params[:register])
-
-    respond_to do |format|
-      if @register.save
-        format.html { redirect_to @register, notice: 'Register was successfully created.' }
-        format.json { render json: @register, status: :created, location: @register }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @register.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /registers/1
-  # PUT /registers/1.json
-  def update
-    @register = Register.find(params[:id])
-
-    respond_to do |format|
-      if @register.update_attributes(params[:register])
-        format.html { redirect_to @register, notice: 'Register was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @register.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /registers/1
-  # DELETE /registers/1.json
-  def destroy
-    @register = Register.find(params[:id])
-    @register.destroy
-
-    respond_to do |format|
-      format.html { redirect_to registers_url }
-      format.json { head :ok }
     end
   end
 end
