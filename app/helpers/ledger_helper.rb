e9ed9098db2ba1db51de2e0b4311f982f9c21452
Andrew Frank <andrewpaulfrank@gmail.com>
2012-03-02 12:23:55 -0500

    Refactored ledger display logic out of the view Created helper method to handle ledger display logic Created view partials to generate each of the different types of rows

--- app/helpers/ledger_helper.rb
+++ app/helpers/ledger_helper.rb
@@ -1,2 +1,20 @@
 module LedgerHelper
+	def ledger_builder(record)
+    case record
+    when Payment
+      if record.user_id == current_user.id
+        render :partial => 'payment_user_paid', :locals => {:record => record}
+      elsif record.receiver_id == current_user.id
+      	render :partial => 'payment_user_received', :locals => {:record => record}
+      else
+      	"This payment record shouldn't be here"
+      end
+    when Expense
+      render :partial => 'expense', :locals => {:record => record}
+    when YouOweMe
+     	render :partial => 'you_owe_me', :locals => {:record => record}
+    else
+      "ledger_builder doesn't work with objects of class #{record.class}"
+    end
+  end
 end
