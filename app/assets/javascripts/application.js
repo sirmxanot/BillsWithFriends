7f46de1d92dba59588002abb097781e50f022add
Andrew Frank <andrewpaulfrank@gmail.com>
2012-03-16 13:52:20 -0400

    fixed remove_fields javascript function. update selector mechanism

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 
 function remove_fields (link) {
 	$(link).prev("input[type=hidden]").val("1");
-	$(link).closest(".fields").hide();
+	$(link).closest(".row-fluid").hide();
 }
 
 function add_fields(link, association, content) {
