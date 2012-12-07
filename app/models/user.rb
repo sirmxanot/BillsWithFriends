c5fa0d6b7717782ac3b76d5729db0fa4ef9b3c0f
Andrew Frank <andrewpaulfrank@gmail.com>
2012-12-07 19:42:55 -0300

    fixed typos in views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,21 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, 
+  #:timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
+  attr_accessible :email, :password, :password_confirmation, 
+                  :remember_me, :first_name, :last_name
 
   has_one  :register
   has_many :expenses
   has_many :payments
   has_many :you_owe_mes
 
-  validates_presence_of :first_name, :last_name, :email, :password, :password_confirmation
+  validates_presence_of :first_name, :last_name, :email, 
+                        :password, :password_confirmation
   validates :email, :uniqueness => true
 
   public
